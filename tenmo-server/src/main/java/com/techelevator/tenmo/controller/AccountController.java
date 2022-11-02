c5fd006e2d8c2946d963466159833e2674568438
Chad Delmontiano <chad.a.delmont@gmail.com>
2022-11-02 15:47:46 -0400

    ThisandTHat

--- tenmo-server/src/main/java/com/techelevator/tenmo/controller/AccountController.java
+++ tenmo-server/src/main/java/com/techelevator/tenmo/controller/AccountController.java
@@ -3,13 +3,12 @@ package com.techelevator.tenmo.controller;
 import com.techelevator.tenmo.dao.UserDao;
 import com.techelevator.tenmo.model.User;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.parameters.P;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 @RestController
 public class AccountController {
@@ -22,6 +21,17 @@ public class AccountController {
     public BigDecimal getBalance (@PathVariable int id){
         BigDecimal balance = userDao.getBalanceById(id);
         return balance;
+    }
+
+    @RequestMapping(path = "account", method = RequestMethod.GET)
+    public List<User> userList(){
+        return userDao.findAll();
+    }
+
+    //TransferTo
+    @RequestMapping(path = "account/{id}/balance", method = RequestMethod.PUT)
+    public void transferAmount (@RequestBody BigDecimal transferAmount, @PathVariable int fromId, int toId){
+        userDao.transferTo(fromId, toId, transferAmount);
     }
 
 
--- tenmo-server/src/main/java/com/techelevator/tenmo/dao/UserDao.java
+++ tenmo-server/src/main/java/com/techelevator/tenmo/dao/UserDao.java
@@ -7,6 +7,8 @@ import java.util.List;
 
 public interface UserDao {
 
+    void transferTo (int fromId, int toId, BigDecimal transferAmount);
+
     List<User> findAll();
 
     BigDecimal getBalanceById (int userId);
